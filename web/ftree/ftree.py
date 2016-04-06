e592ea68d87d8002ef120143ea918cf51b380705
Tobias Stenzel <tobias.stenzel@tum.de>
2016-04-06 06:59:55 +0000

    patch ftree

--- web/ftree/ftree.py
+++ web/ftree/ftree.py
@@ -17,8 +17,9 @@
  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 import logging
-from core import Node
 from core import db
+from core.transition import current_user
+from contenttypes import Container, Content
 
 from .ftreedata import getData, getPathTo, getLabel
 
@@ -27,6 +28,11 @@ logg = logging.getLogger(__name__)
 
 
 def ftree(req):
+
+    if not current_user.is_editor:
+        logg.warn("ftree permission denied for user: %s", current_user.id)
+        return 403
+
     if "parentId" in req.params:
         return getData(req)
 
@@ -38,14 +44,19 @@ def ftree(req):
 
     if "changeCheck" in req.params:
         for id in req.params.get("currentitem").split(","):
-            node = q(Node).get(id)
-            parent = q(Node).get(req.params.get("changeCheck"))
-            if node in parent.children:
+            node = q(Content).get(id)
+            parent = q(Container).get(req.params.get("changeCheck"))
+            if not(node and parent and node.has_write_access() and parent.has_write_access()):
+                logg.warn("illegal ftree request: %s", req.params)
+                return 403
+
+            if node in parent.content_children:
                 if len(node.parents) > 1:
-                    parent.children.remove(node)
+                    parent.content_children.remove(node)
+                    logg.info("ftree change ")
                     db.session.commit()
                 else:
                     req.writeTALstr('<tal:block i18n:translate="edit_classes_noparent"/>', {})
             else:
-                parent.children.append(node)
+                parent.content_children.append(node)
                 db.session.commit()
