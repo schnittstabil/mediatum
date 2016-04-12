b0bacd95902863b7efa4a6171c829b7f06f575c9
Werner Neudenberger <werner.neudenberger@tum.de>
2016-04-12 17:47:51 +0200

    new editor will only edit acls for one node at a time

--- web/edit/modules/acls.py
+++ web/edit/modules/acls.py
@@ -113,18 +113,17 @@ def getContent(req, ids):
         req.setStatus(httpstatus.HTTP_FORBIDDEN)
         return req.getTAL("web/edit/edit.html", {}, macro="access_error")
 
-    # check write access to nodes
-    nodes = []
-    for nid in ids:
-        node = q(Node).get(nid)
-        if node.has_write_access():
-            nodes.append(node)
-        else:
-            req.setStatus(httpstatus.HTTP_FORBIDDEN)
-            return req.getTAL("web/edit/edit.html", {}, macro="access_error")
+    if len(ids) != 1:  # should not happen
+        req.setStatus(httpstatus.HTTP_FORBIDDEN)
+        return req.getTAL("web/edit/edit.html", {}, macro="access_error")
 
-
-    idstr = ",".join(ids)
+    # check write access to node
+    idstr = ids[0]
+    nid = long(idstr)
+    node = q(Node).get(nid)
+    if not node.has_write_access():
+        req.setStatus(httpstatus.HTTP_FORBIDDEN)
+        return req.getTAL("web/edit/edit.html", {}, macro="access_error")
 
     if "save" in req.params:
         logg.info("%r change access %r", current_user, idstr)
@@ -134,28 +133,26 @@ def getContent(req, ids):
 
                 ruleset_names_from_request = [rsn for rsn in req.params.get(u"left%s" % rule_type, u"").split(u";") if rsn.strip()]
 
-                for node in nodes:
+                rules_info_dict = make_access_rules_info_dict(node, rule_type)
+                rulesets_not_inherited = rules_info_dict.get('rulesets_not_inherited', [])
 
-                    rules_info_dict = make_access_rules_info_dict(node, rule_type)
-                    rulesets_not_inherited = rules_info_dict.get('rulesets_not_inherited', [])
+                ruleset_names_not_inherited = [rs_dict.get('ruleset_name') for rs_dict in rulesets_not_inherited]
 
-                    ruleset_names_not_inherited = [rs_dict.get('ruleset_name') for rs_dict in rulesets_not_inherited]
+                to_be_removed_rulesets = set(ruleset_names_not_inherited) - set(ruleset_names_from_request)
+                to_be_added_rulesets = set(ruleset_names_from_request) - set(ruleset_names_not_inherited) - {'__special_rule__'}
 
-                    to_be_removed_rulesets = set(ruleset_names_not_inherited) - set(ruleset_names_from_request)
-                    to_be_added_rulesets = set(ruleset_names_from_request) - set(ruleset_names_not_inherited) - {'__special_rule__'}
+                if to_be_removed_rulesets:
+                    msg = "node %r: %r removing rulesets %r" % (node, rule_type, to_be_removed_rulesets)
+                    logg.info(msg)
+                    for ruleset_name in to_be_removed_rulesets:
+                        node.access_ruleset_assocs.filter_by(ruleset_name=ruleset_name,
+                                                             ruletype=rule_type).delete()
 
-                    if to_be_removed_rulesets:
-                        msg = "node %r: %r removing rulesets %r" % (node, rule_type, to_be_removed_rulesets)
-                        logg.info(msg)
-                        for ruleset_name in to_be_removed_rulesets:
-                            node.access_ruleset_assocs.filter_by(ruleset_name=ruleset_name,
-                                                                 ruletype=rule_type).delete()
-
-                    if to_be_added_rulesets:
-                        msg = "node %r: %r adding rulesets %r" % (node, rule_type, to_be_added_rulesets)
-                        logg.info(msg)
-                        for ruleset_name in to_be_added_rulesets:
-                            node.access_ruleset_assocs.append(NodeToAccessRuleset(ruleset_name=ruleset_name, ruletype=rule_type))
+                if to_be_added_rulesets:
+                    msg = "node %r: %r adding rulesets %r" % (node, rule_type, to_be_added_rulesets)
+                    logg.info(msg)
+                    for ruleset_name in to_be_added_rulesets:
+                        node.access_ruleset_assocs.append(NodeToAccessRuleset(ruleset_name=ruleset_name, ruletype=rule_type))
 
             db.session.commit()
 
@@ -169,42 +166,40 @@ def getContent(req, ids):
 
                 user_ids_from_request = [rsn for rsn in req.params.get(u"leftuser%s" % rule_type, u"").split(u";") if rsn.strip()]
 
-                for node in nodes:
+                rules_info_dict = make_access_rules_info_dict(node, rule_type)
+                rulesets_not_inherited = rules_info_dict.get('rulesets_not_inherited', [])
 
-                    rules_info_dict = make_access_rules_info_dict(node, rule_type)
-                    rulesets_not_inherited = rules_info_dict.get('rulesets_not_inherited', [])
+                additional_rules[rule_type] = rules_info_dict.get('additional_rules', [])
+                additional_rules_inherited[rule_type] = [rd for rd in additional_rules[rule_type] if rd.get('inherited', False)]
+                additional_rules_not_inherited[rule_type] = [rd for rd in additional_rules[rule_type] if not rd.get('inherited', False)]
 
-                    additional_rules[rule_type] = rules_info_dict.get('additional_rules', [])
-                    additional_rules_inherited[rule_type] = [rd for rd in additional_rules[rule_type] if rd.get('inherited', False)]
-                    additional_rules_not_inherited[rule_type] = [rd for rd in additional_rules[rule_type] if not rd.get('inherited', False)]
+                uids = []
+                for ar_dict in additional_rules_not_inherited[rule_type]:
+                    ar_id = ar_dict.get('rule_id')
+                    ar = q(AccessRule).get(ar_id)
+                    test_result = decider_is_private_user_group_access_rule(ar)
+                    if not type(test_result) == User:
+                        continue
+                    elif not test_result.id in user_ids_from_request:
+                        assoc = node.access_rule_assocs.filter_by(rule_id=ar_id, ruletype=rule_type).scalar()
+                        node.access_rule_assocs.remove(assoc)
+                    else:
+                        uids.append(test_result.id)
 
-                    uids = []
-                    for ar_dict in additional_rules_not_inherited[rule_type]:
-                        ar_id = ar_dict.get('rule_id')
-                        ar = q(AccessRule).get(ar_id)
-                        test_result = decider_is_private_user_group_access_rule(ar)
-                        if not type(test_result) == User:
-                            continue
-                        elif not test_result.id in user_ids_from_request:
-                            assoc = node.access_rule_assocs.filter_by(rule_id=ar_id, ruletype=rule_type).scalar()
-                            node.access_rule_assocs.remove(assoc)
-                        else:
-                            uids.append(test_result.id)
-
-                    for uid in user_ids_from_request:
-                        if uid in uids:
-                            continue
-                        # add access rule
-                        user = q(User).get(uid)
-                        pug_id = user.private_group_id
-                        ar = q(AccessRule).filter(AccessRule.group_ids.all(pug_id))\
-                              .filter_by(subnets=None, dateranges=None,
-                                         invert_group=False, invert_date=False, invert_subnet=False)\
-                              .scalar()
-                        if not ar:
-                            ar = AccessRule(group_ids = [pug_id])
-                        assoc = NodeToAccessRule(rule=ar, ruletype=rule_type, invert=False, blocking=False)
-                        node.access_rule_assocs.append(assoc)
+                for uid in user_ids_from_request:
+                    if uid in uids:
+                        continue
+                    # add access rule
+                    user = q(User).get(uid)
+                    pug_id = user.private_group_id
+                    ar = q(AccessRule).filter(AccessRule.group_ids.all(pug_id))\
+                          .filter_by(subnets=None, dateranges=None,
+                                     invert_group=False, invert_date=False, invert_subnet=False)\
+                          .scalar()
+                    if not ar:
+                        ar = AccessRule(group_ids = [pug_id])
+                    assoc = NodeToAccessRule(rule=ar, ruletype=rule_type, invert=False, blocking=False)
+                    node.access_rule_assocs.append(assoc)
 
             db.session.commit()
 
@@ -222,9 +217,8 @@ def getContent(req, ids):
         runsubmit += "\tmark(document.myform.left" + rule_type + ");\n"
         runsubmit += "\tmark(document.myform.leftuser" + rule_type + ");\n"
 
-        for node in nodes:
-            rules_info_dict = make_access_rules_info_dict(node, rule_type)
-            logg.debug("node: %r, rule_type: %r, info_dict: %r" % (node, rule_type, rules_info_dict))
+        rules_info_dict = make_access_rules_info_dict(node, rule_type)
+        logg.debug("node: %r, rule_type: %r, info_dict: %r" % (node, rule_type, rules_info_dict))
 
         rulesets_not_inherited = rules_info_dict.get('rulesets_not_inherited', [])
         not_inherited_ruleset_names[rule_type] = [r.get('ruleset_name', '-unnamed-ruleset-') for r in rulesets_not_inherited]
