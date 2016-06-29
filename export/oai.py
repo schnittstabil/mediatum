3bc076e7791bb7e0f0d4a0c05f1baec7602df585
Werner Neudenberger <werner.neudenberger@tum.de>
2016-06-29 11:15:20 +0200

    remove out commented code

--- export/oai.py
+++ export/oai.py
@@ -243,10 +243,6 @@ def ListMetadataFormats(req):
             node = q(Node).get(nid)
         except (TypeError, KeyError):
             return writeError(req, "badArgument")
-        #except Exception as e:
-        #    print type(e)
-        #    print e
-        #    return writeError(req, "badArgument")
         if node is None:
             return writeError(req, "badArgument")
 
