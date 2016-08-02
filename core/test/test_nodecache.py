b5957e8dbf946ccbffe88deae5d5ef20de0bbfeb
Tobias Stenzel <tobias.stenzel@tum.de>
2016-08-02 20:42:21 +0000

    add in-memory node caching for common singleton nodes
    Home, Root and Collections are fetched in many places
    but don't change during runtime (we assume).
    Caching them in memory is a good idea but we must be careful about
    detached objects. To make it safe, the object is merged with the current
    session all time when requested. This is quite slow and should be done
    only once per request.

--- core/test/test_nodecache.py
+++ core/test/test_nodecache.py
@@ -1,40 +1,12 @@
 # -*- coding: utf-8 -*-
 """
-    :copyright: (c) 2014 by the mediaTUM authors
+    :copyright: (c) 2016 by the mediaTUM authors
     :license: GPL3, see COPYING for details
 """
-from pytest import fixture
-
-from core import db
-from core.test.asserts import assert_node
-from core.permission import get_or_add_everybody_rule
-q = db.query
+from __future__ import absolute_import
+from core.nodecache import get_collections_node
 
 
-@fixture(autouse=True)
-def default_data(default_data):
-    return default_data
-
-
-def test_get_root():
-    from core.systemtypes import Root
-    n = q(Root).one()
-    assert_node(n,
-                name="root",
-                type="root",
-                schema=None)
-
-
-def test_get_collections():
-    from contenttypes import Collections
-    n = q(Collections).one()
-    assert_node(n,
-                name="collections",
-                type="collections",
-                schema="collection",
-                label="Collections")
-
-    # collections is public by default, so it must have the everybody rule
-    everybody_rule = get_or_add_everybody_rule()
-    assert n.access_rule_assocs[0].rule is everybody_rule
-
+def test_get_collections_node(req, collections):
+    cached_collections = get_collections_node()
+    assert cached_collections == collections
