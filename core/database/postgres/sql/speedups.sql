a8f0bcc9bbcdd5e28c6e8eb7490204f9fc99f1d1
Tobias Stenzel <tobias.stenzel@tum.de>
2016-08-22 20:04:18 +0000

    sql: speed up container children counting
    * use a materialized view to cache child counts (used for navtree)

--- core/database/postgres/sql/speedups.sql
+++ core/database/postgres/sql/speedups.sql
@@ -27,3 +27,29 @@ RETURN QUERY SELECT
     AND has_read_access_to_node(nm.nid, group_ids, ipaddr, date);
 END;
 $f$;
+
+
+CREATE MATERIALIZED VIEW IF NOT EXISTS :search_path.container_info AS 
+SELECT 
+    id AS nid
+    ,(SELECT count(*) FROM :search_path.node JOIN :search_path.noderelation nr ON (id=cid) 
+      WHERE nid=no.id 
+      AND subnode=false
+      AND type IN (SELECT name FROM nodetype WHERE is_container=false)) AS count_content_children_for_all_subcontainers
+
+FROM :search_path.node no WHERE no.type IN (SELECT name FROM nodetype WHERE is_container=true);
+
+
+CREATE OR REPLACE FUNCTION count_content_children_for_all_subcontainers(container_id integer)
+    RETURNS integer
+    LANGUAGE plpgsql
+    SET search_path = :search_path
+    IMMUTABLE
+    AS $f$
+DECLARE 
+    cc integer;
+BEGIN
+    SELECT count_content_children_for_all_subcontainers INTO cc FROM container_info WHERE nid=container_id;
+    RETURN cc;
+END;
+$f$;
