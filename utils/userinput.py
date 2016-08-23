cc824cb8cfb43654b7b9e581afd5c455c79960dd
Tobias Stenzel <tobias.stenzel@tum.de>
2016-08-23 07:27:55 +0000

    warn if None is passed to userinput.string_to_int, better log msg for illegal values

--- utils/userinput.py
+++ utils/userinput.py
@@ -13,10 +13,13 @@ logg = logging.getLogger(__name__)
 
 
 def string_to_int(data):
+    if data is None:
+        logg.warn("cannot convert None value to an integer")
+        return None
     try:
         nid = int(data)
     except ValueError:
-        logg.warn("invalid user input for int: %s", data)
+        logg.warn("invalid user input for int: '%r'", data)
         return None
 
     return nid
