c1c40253fac29d12e51c3ff0e8e9783980c75f0b
Tobias Stenzel <tobias.stenzel@tum.de>
2016-09-01 19:50:25 +0000

    sql: fix_transitive_integrity adds / removes missing / excess transitive tuples

--- core/database/postgres/sql/noderelation_funcs_transitive_integrity.sql
+++ core/database/postgres/sql/noderelation_funcs_transitive_integrity.sql
@@ -14,6 +14,24 @@ RETURN QUERY
 END;
 $$;
 
+CREATE OR REPLACE FUNCTION fix_transitive_integrity() RETURNS SETOF integrity_check_noderelation
+    LANGUAGE plpgsql
+    SET search_path = :search_path
+    AS $$
+BEGIN
+RETURN QUERY
+    WITH closure AS (SELECT * FROM transitive_closure())
+    , excess AS (SELECT * FROM noderelation EXCEPT SELECT * from closure)
+    , missing AS (SELECT * FROM closure EXCEPT SELECT * from noderelation)
+    , ins AS (INSERT INTO noderelation SELECT * FROM missing)
+    , del AS (DELETE FROM noderelation nr WHERE (nr.nid, nr.cid, nr.distance) IN (SELECT * FROM excess))
+
+    (SELECT *, 'missing' FROM missing)
+    UNION ALL
+    (SELECT *, 'excess' FROM excess);
+END;
+$$;
+
 CREATE OR REPLACE FUNCTION compare_noderelation_with_backup() RETURNS SETOF integrity_check_noderelation
     LANGUAGE plpgsql
     SET search_path = :search_path
